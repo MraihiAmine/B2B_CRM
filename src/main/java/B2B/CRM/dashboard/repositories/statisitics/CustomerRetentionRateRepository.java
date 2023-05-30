fea1c759dc629bccaea1bd39717b0aee118349b4
Mraihi Amine <mraihiamin@gmail.com>
2023-05-30 07:40:09 +0100

    SSP-46 Addition of a service that get the user role using this role to modify the sidebar depending on the role

--- src/main/java/B2B/CRM/dashboard/repositories/statisitics/CustomerRetentionRateRepository.java
+++ src/main/java/B2B/CRM/dashboard/repositories/statisitics/CustomerRetentionRateRepository.java
@@ -1,11 +1,18 @@
 package B2B.CRM.dashboard.repositories.statisitics;
+
 import B2B.CRM.dashboard.entities.statistics.CustomerRetentionRateEntity;
+import B2B.CRM.dashboard.entities.statistics.Product;
+import B2B.CRM.dashboard.entities.statistics.YearStatistic;
+import java.time.LocalDate;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
-import java.util.Optional;
-
 @Repository("CustomerRetentionRateRepository")
-public interface CustomerRetentionRateRepository extends JpaRepository<CustomerRetentionRateEntity, Long> {
+public interface CustomerRetentionRateRepository
+  extends JpaRepository<CustomerRetentionRateEntity, Long> {
+  Optional<CustomerRetentionRateEntity> findByYearStatisticAndProduct(
+    YearStatistic yearStatistic,
+    Product product
+  );
 }
